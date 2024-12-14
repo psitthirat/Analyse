15864ff1440b41022bd3ea597cb616af8ad3bf7b
psitthirat <162864308+psitthirat@users.noreply.github.com>
2024-12-14 19:03:41 +0700

    Create web scraping function

---
+++ scripts/scrp_spc.py
@@ -0,0 +1,191 @@
+import os
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from datetime import datetime
+import time
+
+def scrap(hosp_id, spc_link, output_folder):
+    """
+    Scrape specialty data from a specified health map website and save it as a CSV file.
+
+    Parameters:
+    - hosp_id (DataFrame): A DataFrame containing hospital IDs.
+    - spc_link (DataFrame): A DataFrame containing specialty links.
+    - output_folder (str): The directory to save the output files.
+
+    Returns:
+    - str: The path to the saved CSV file.
+    """
+    spc_df = pd.DataFrame()
+    N = 1
+
+    # Ensure the output folder exists
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Initialize tqdm progress bar
+    with tqdm(total=len(hosp_id), desc="Progress") as pbar:
+        for index_hosp, row_hosp in hosp_id.iterrows():
+            for index_link, row_link in spc_link.iterrows():
+                
+                # Extract the specialty data from URL
+                hosp_url = f"http://gishealth.moph.go.th/healthmap/{row_link['link']}.php?maincode={row_hosp['hosp_id']}"
+
+                if row_link['link'] == 'infopersonal':
+                    n_select = 3
+                    n_table = 5
+                else:
+                    n_select = 2
+                    n_table = 4
+                
+                success = False
+                while not success:
+                    
+                    try:
+                        # GET request to the URL with retry mechanism
+                        response_hosp = requests.get(hosp_url)
+                        success = True
+                    except requests.exceptions.RequestException as e:
+                        print(f"Failed to fetch data for hosp_id {row_hosp['hosp_id']} with error: {e}")
+                        print("Retrying...")
+                        time.sleep(5)  # Wait before retrying
+                    
+                        
+                # Parse the HTML content of the page
+                soup_hosp = BeautifulSoup(response_hosp.text, 'html.parser')
+
+                # Find all tables in the updated page
+                tables = soup_hosp.find_all('table')
+
+                # Check the availability of tables
+                check_table = tables[n_select]
+                select_element = check_table.find('select')
+                options = select_element.find_all('option')
+                check_data = [{'id': option.get('value'), 'check': option.text.strip()} for option in options]
+
+                        
+                # Check if "ไม่มีการบันทึกข้อมูล"
+                if check_data[0]['check'] != "ไม่มีการบันทึกข้อมูล":
+                            
+                    for point in check_data:
+                        
+                        id = point['id']
+                        time_input = point['check']
+                        
+                        point_url = f"{hosp_url}&id={id}"
+                        
+                        success = False
+                        while not success:
+                            try:
+                                response_point = requests.get(point_url)
+                                success = True
+                            except requests.exceptions.RequestException as e:
+                                print(f"Failed to fetch data for hosp_id {row_hosp['hosp_id']} with error: {e}")
+                                print("Retrying...")
+                                time.sleep(5)
+                        
+                        soup_point = BeautifulSoup(response_point.text, 'html.parser')
+                        tables_point = soup_point.find_all('table')
+
+                        # Choose specialist table
+                        spc_table = tables_point[n_table]
+
+                    
+                        # Your scraping logic for table goes here
+                        rows = spc_table.find_all('tr')
+                        spc = []
+                        for row_value in rows:
+                            columns_value = row_value.find_all(['td'])[0:7]
+                            row_data_value = [column_value.get_text(strip=True) for column_value in columns_value]
+                            spc.append(row_data_value)
+                    
+                        # Convert the scraped data into a DataFrame
+                        spc_hos_df = pd.DataFrame(spc)
+
+                        # Assign the first row as the header
+                        spc_hos_df.columns = spc_hos_df.iloc[0]
+                        spc_hos_df = spc_hos_df[1:]  
+                        spc_hos_df = spc_hos_df.loc[~(spc_hos_df.iloc[:, 0].eq('') | spc_hos_df.iloc[:, 0].eq('ลำดับ'))]
+                        
+                        # Check if the hospital is 'public' or 'private' from number of column
+                        if row_link['link'] == 'infopersonal':
+                            if spc_hos_df.columns.str.contains('ข้าราชการ').any():
+                                spc_hos_df = spc_hos_df.iloc[0:1, 1:6]
+                                spc_hos_df.iloc[:, 1:5] = spc_hos_df.iloc[:, 1:5].apply(pd.to_numeric, errors='coerce')
+                                spc_hos_df['total'] = spc_hos_df.iloc[:, 1:5].apply(lambda row: row.sum() if not row.isnull().all() else None, axis=1)
+                                spc_hos_df = spc_hos_df.drop(spc_hos_df.columns[1:5], axis=1)
+                            else:
+                                spc_hos_df = spc_hos_df.iloc[0:1, 1:3]
+                                spc_hos_df.rename(columns=lambda x: x.replace('FT', 'total') if x.startswith('FT') else x, inplace=True)
+                                spc_hos_df['total'] = spc_hos_df['total'].apply(
+                                    lambda x: int(x) if pd.notnull(x) and x != '' else pd.NA)
+                        else:
+                            if len(spc_hos_df.columns) == 4:
+                                spc_hos_df = spc_hos_df.iloc[:, 1:3]
+                                spc_hos_df.rename(columns=lambda x: x.replace('Full Time', 'total') if x.startswith('Full Time') else x, inplace=True)
+                                spc_hos_df['total'] = spc_hos_df['total'].apply(
+                                    lambda x: int(x) if pd.notnull(x) and x != '' else pd.NA)
+                            elif len(spc_hos_df.columns) == 7:
+                                spc_hos_df = spc_hos_df.iloc[:, 1:6]
+                                spc_hos_df.iloc[:, 1:6] = spc_hos_df.iloc[:, 1:6].apply(pd.to_numeric, errors='coerce')
+                                spc_hos_df['total'] = spc_hos_df.iloc[:, 1:6].apply(lambda row: row.sum() if not row.isnull().all() else None, axis=1)
+                                spc_hos_df = spc_hos_df.drop(spc_hos_df.columns[1:5], axis=1)
+                        
+                        spc_hos_df = spc_hos_df.rename(columns={spc_hos_df.columns[0]: 'spc'})        
+                        spc_hos_df.insert(0, 'hosp_id', row_hosp['hosp_id'])
+                        spc_hos_df['time'] = time_input
+                        spc_df = pd.concat([spc_df, spc_hos_df], ignore_index=True)
+            
+        
+            P = (N/len(hosp_id))*100
+
+            # Update progress bar description
+            pbar.set_description(f"Progress {N}/{len(hosp_id)} hcode = {row_hosp['hosp_id']}")
+            N = N+1
+
+            # Save the DataFrame every 100 hosp_id and delete old file
+            if N % 100 == 0:
+                partial_file_path = f'{output_folder}/spc_latest.csv'
+                if os.path.exists(partial_file_path):
+                    os.remove(partial_file_path)
+                spc_df.to_csv(partial_file_path, index=False)
+
+            # Update progress bar
+            pbar.update(1)
+
+    # Delete the partial file before saving the final CSV
+    partial_file_path = os.path.join(output_folder, 'spc_latest.csv')
+    if os.path.exists(partial_file_path):
+        os.remove(partial_file_path)
+
+    final_file_path = os.path.join(output_folder, 'spc.csv')
+    spc_df.to_csv(final_file_path, index=False)
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    # Update or create the readme.txt file with new scraping information
+    readme_path = os.path.join(output_folder, 'readme.txt')
+    with open(readme_path, 'w') as readme_file:
+        readme_file.write(f"\n---------------------------------------\n")
+        readme_file.write(f"Specialty Data Scraping\n")
+        readme_file.write(f"Data Source: http://gishealth.moph.go.th/healthmap/\n")
+        readme_file.write(f"Last Updated: {timestamp}\n")
+        readme_file.write(f"\n---------------------------------------\n")
+        readme_file.write(f"Data have been scraped from multiple hospitals and processed into a clean format.\n")
+        readme_file.write(f"The resulting CSV file ('spc.csv') contains data on medical specialties by hospital.\n")
+
+    print(f"Scraped data have been successfully saved to '{final_file_path}'\n(Last updated: {timestamp}).")
+    
+    return final_file_path
+
+# Example usage in Jupyter Notebook:
+if __name__ == "__main__":
+    # Example DataFrames for hosp_id and spc_link
+    hosp_id_example = pd.DataFrame({'hosp_id': ['001', '002']})
+    spc_link_example = pd.DataFrame({'link': ['infopersonal', 'infohospital']})
+    output_dir = "./output"
+    
+    # Run the scraper
+    output_file = scrap(hosp_id_example, spc_link_example, output_dir)
+    print(f"Scraped data is available in: {output_file}")
